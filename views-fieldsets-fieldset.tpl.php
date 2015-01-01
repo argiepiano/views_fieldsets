aa960a62f9d5f2ced203fc8be82520c2699dfe54
Rudie Dirkx <drupal@hotblocks.nl>
2015-01-01 16:24:45 +0100

    Issue #2291769: Missing fields separator for inline fields inside a fieldset

--- views-fieldsets-fieldset.tpl.php
+++ views-fieldsets-fieldset.tpl.php
@@ -4,16 +4,16 @@
   <?php if ($legend_tag): ?>
     <<?php print $legend_tag; ?>>
       <span class="fieldset-legend"><?php print $legend; ?></span>
-	</<?php print $legend_tag; ?>>
+    </<?php print $legend_tag; ?>>
     <div class="fieldset-wrapper">
   <?php endif; ?>
 
   <?php foreach ($fieldset_fields as $name => $field): ?>
-	<?php print $field->wrapper_prefix . $field->label_html . $field->content . $field->wrapper_suffix; ?>
+    <?php print @$field->separator . $field->wrapper_prefix . $field->label_html . $field->content . $field->wrapper_suffix; ?>
   <?php endforeach; ?>
 
   <?php if ($legend_tag): ?>
-    </div>
+    </div><?php /* .fieldset-wrapper */ ?>
   <?php endif; ?>
 
 </<?php print $tag; ?>>
