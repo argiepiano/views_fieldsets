2ed4f2290661525313a30f04605954f8246115fb
pifagor <pifagor87@gmail.com>
2018-02-12 01:00:12 +0200

    Fixed coding standards and errors.

--- views_fieldsets.api.php
+++ views_fieldsets.api.php
@@ -12,11 +12,9 @@ function test_theme() {
     'views_fieldsets_simple' => array(
       // Label, as seen in Views Field edit screen.
       'views_fieldsets_label' => 'Super simple, func, no tpl',
-
       // Only 'fieldset_fields' is mandatory, but you can add anything you
       // want. 'legend' is optional, but always filled.
       'variables' => array('fieldset_fields' => array(), 'legend' => ''),
-
       // If your hook is a template, you'll need these, but our example isn't.
       // 'template' => 'views-fieldsets-simple',
       // 'path' => $path,
@@ -63,11 +61,10 @@ function hook_views_fieldsets_views_field_form_alter(&$form, $handler) {
 function template_preprocess_views_fieldsets_simple(&$variables) {
   // Much better legend:
   $variables['legend'] = 'OVERRIDE YO!';
-
   // Wrap the whole thing with a link:
-  $handler = $vars['fields'][ $vars['fieldset_field'] ]->handler;
+  $handler = $variables['fields'][$variables['fieldset_field']]->handler;
   $path = $handler->options['fieldset']['link']['path'];
-  $path = $handler->tokenize_value($path, $vars['view']->row_index);
+  $path = $handler->tokenize_value($path, $variables['view']->row_index);
   $vars['href'] = url($path); //  Use $href in the custom tpl or theme function.
 }
 
@@ -75,14 +72,12 @@ function template_preprocess_views_fieldsets_simple(&$variables) {
  * Theme function for 'views_fieldsets_simple'.
  */
 function theme_views_fieldsets_simple($variables) {
-  $content = implode("\n", array_map(function($field) {
-    return @$field->separator . $field->wrapper_prefix . $field->label_html . $field->content . $field->wrapper_suffix;
+  $content = implode("\n", array_map(function ($field) {
+    return $field->separator . $field->wrapper_prefix . $field->label_html . $field->content . $field->wrapper_suffix;
   }, $variables['fieldset_fields']));
-
-  $html  = '<fieldset>';
+  $html = '<fieldset>';
   $html .= '<legend>' . check_plain(strip_tags($variables['legend'])) . '</legend>';
   $html .= check_plain(strip_tags($content)); // Wrap this in $href maybe?
   $html .= '</fieldset>';
-
   return $html;
 }
